d4047db9f0db4268d2e2345629755b81229bc804
arielmartinelli <arielmartinelli2019@gmail.com>
2025-01-03 13:12:16 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.getElementById('reveal-btn').addEventListener('click', () => {
+    // Ocultar la primera parte de la página
+    document.getElementById('main-content').style.display = 'none';
+
+    // Mostrar la segunda parte
+    document.querySelector('.hidden-content').style.display = 'block';
+});
+
+function sendToWhatsApp() {
+    const date = document.getElementById('date').value;
+    const plan = document.getElementById('plan').value;
+    const phoneNumber = '3516121498';
+    const message = `Hola, quiero organizar un día troll el ${date} con el plan: ${plan}`;
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.location.href = whatsappURL;
+}
+
